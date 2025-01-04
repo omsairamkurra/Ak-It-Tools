8cc7eef870056fcb838eff938de90b44a531e676
omsairamkurra <kurraomsairam2@gmail.com>
2025-01-04 22:32:04 +0530

    first commit

---
+++ src/sidebar/sidebar.js
@@ -0,0 +1,207 @@
+import React, { useState } from "react";
+import "./sidebar.css"; // Make sure this file exists for styling.
+import TokenIcon from '@mui/icons-material/Token';
+import TagIcon from '@mui/icons-material/Tag';
+import EnhancedEncryptionIcon from '@mui/icons-material/EnhancedEncryption';
+import HideSourceIcon from '@mui/icons-material/HideSource';
+import Rotate90DegreesCcwIcon from '@mui/icons-material/Rotate90DegreesCcw';
+import LockIcon from '@mui/icons-material/Lock';
+import MenuIcon from '@mui/icons-material/Menu';
+import DragHandleIcon from '@mui/icons-material/DragHandle';
+import KeyIcon from '@mui/icons-material/Key';
+import PasswordIcon from '@mui/icons-material/Password';
+import PictureAsPdfIcon from '@mui/icons-material/PictureAsPdf';
+
+//convertor
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+import CompareArrowsIcon from '@mui/icons-material/CompareArrows';
+import Looks3Icon from '@mui/icons-material/Looks3';
+
+const Sidebar = () => {
+    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+    const toggleSidebar = () => {
+        setIsSidebarOpen(!isSidebarOpen);
+    };
+
+    const toggleDropdown = (id) => {
+        const dropdown = document.getElementById(id);
+        if (dropdown) {
+            dropdown.classList.toggle('hidden'); // Show/hide dropdown
+        }
+    };
+
+    return (
+        <div className="app">
+            {/* Hamburger Icon */}
+            <div className="hamburger" onClick={toggleSidebar}>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+
+            {/* Sidebar */}
+            <div className={`sidebar ${isSidebarOpen ? "open" : ""}`}>
+                <div class="tools-header">
+                    <h3>AK-IT-Tools</h3>
+                    <p>Handy tools for developers.</p>
+                </div>
+                <div class="container">
+                    <div className="item" onClick={() => toggleDropdown('crypto-dropdown')}>
+                        <span class="symbol">&gt;</span>
+                        <span class="text">Crypto</span>
+                    </div>
+                    <div id="crypto-dropdown" class="dropdown hidden">
+                        <div class="dropdown-item">
+                            <i class="icon">
+                                <TokenIcon />
+                            </i>
+                            <span>Token Generator</span>
+                        </div>
+                        <div class="dropdown-item">
+                            <i class="icon">
+                                <TagIcon />
+                            </i>
+                            <span>Hash Generator</span>
+                        </div>
+                        <div class="dropdown-item">
+                            <i class="icon">
+                                <EnhancedEncryptionIcon />
+                            </i>
+                            <span>Bcrypt</span>
+                        </div>
+
+                        <div class="dropdown-item">
+                            <i class="icon">
+                                <HideSourceIcon />
+                            </i>
+                            <span>UUIDs generator</span>
+                        </div>
+                        <div class="dropdown-item">
+                            <i class="icon">
+                                <Rotate90DegreesCcwIcon />
+                            </i>
+                            <span>ULID Generator</span>
+                        </div>
+                        <div class="dropdown-item">
+                            <i class="icon">
+                                <LockIcon />
+                            </i>
+                            <span>Encrypt / decrypt text</span>
+                        </div>
+
+                        <div class="dropdown-item">
+                            <i class="icon">
+                                <MenuIcon />
+                            </i>
+                            <span>BIP39 passphrase generator</span>
+                        </div>
+                        <div class="dropdown-item">
+                            <i class="icon">
+                                <DragHandleIcon />
+                            </i>
+                            <span>Hmac Generator</span>
+                        </div>
+                        <div class="dropdown-item">
+                            <i class="icon">
+                                <KeyIcon />
+                            </i>
+                            <span>RSA key pair generator</span>
+                        </div>
+
+                        <div class="dropdown-item">
+                            <i class="icon">
+                                <PasswordIcon />
+                            </i>
+                            <span>Password strength analyser</span>
+                        </div>
+                        <div class="dropdown-item">
+                            <i class="icon">
+                                <PictureAsPdfIcon />
+                            </i>
+                            <span>PDF signature checker</span>
+                        </div>
+
+                    </div>
+                    <div className="item" onClick={() => toggleDropdown('converter-dropdown')}>
+                        <span class="symbol">&gt;</span>
+                        <span class="text">Converter</span>
+                    </div>
+                    <div id="converter-dropdown" class="dropdown hidden">
+                        <div class="dropdown-item">
+                            <i class="icon">
+                                <CalendarMonthIcon />
+                            </i>
+                            <span>Date-time converter</span>
+                        </div>
+                        <div class="dropdown-item">
+                            <i class="icon">
+                                <CompareArrowsIcon />
+                            </i>
+                            <span>Integer base converter</span>
+                        </div>
+                        <div class="dropdown-item">
+                            <i class="icon">
+                                <Looks3Icon />
+                            </i>
+                            <span>Roman numeral converter</span>
+                        </div>
+
+                        <div class="dropdown-item">
+                            <i class="icon">
+                                <EnhancedEncryptionIcon />
+                            </i>
+                            <span>Base64 string encoder/decoder</span>
+                        </div>
+                    </div>
+
+                    <div class="item">
+                        <span class="symbol">&gt;</span>
+                        <span class="text">Web</span>
+                    </div>
+                    <div class="item">
+                        <span class="symbol">&gt;</span>
+                        <span class="text">Images and Videos</span>
+                    </div>
+                    <div class="item">
+                        <span class="symbol">&gt;</span>
+                        <span class="text">Development</span>
+                    </div>
+                    <div class="item">
+                        <span class="symbol">&gt;</span>
+                        <span class="text">Network</span>
+                    </div>
+                    <div class="item">
+                        <span class="symbol">&gt;</span>
+                        <span class="text">Math</span>
+                    </div>
+                    <div class="item">
+                        <span class="symbol">&gt;</span>
+                        <span class="text">Measurement</span>
+                    </div>
+                    <div class="item">
+                        <span class="symbol">&gt;</span>
+                        <span class="text">Text</span>
+                    </div>
+                    <div class="item">
+                        <span class="symbol">&gt;</span>
+                        <span class="text">Data</span>
+                    </div>
+                </div>
+
+                <div className="sidebar-footer">
+                    <h3>Ak-IT-Tools © 2025 Ram kurra</h3>
+                </div>
+
+            </div>
+
+            {/* Main Content */}
+            <div className="content">
+                <span>Welcome to the Website</span>
+                <p>Click the hamburger icon to open the sidebar.</p>
+            </div>
+        </div>
+    );
+};
+
+export default Sidebar;
